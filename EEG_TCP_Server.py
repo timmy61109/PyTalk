7f3993fb2292af023811a9f6552b1f5d631932e4
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2018-11-02 11:01:35 +0800

    刪除註解說明
    A simple "tcp echo server" for demonstrating TCP usage.
    The server listens for TCP packets and echoes any received
    packets back to the originating host.
    20180907使用TCP模組來進行程式的簡化，會將內容轉移到System.py

--- EEG_TCP_Server.py
+++ EEG_TCP_Server.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # coding = UTF-8
-"""
-A simple "tcp echo server" for demonstrating TCP usage.
-The server listens for TCP packets and echoes any received
-packets back to the originating host.
-20180907使用TCP模組來進行程式的簡化，會將內容轉移到System.py
-"""
 import TCP
 import FileAccess as FA
 #FileName = "./Data.csv"
@@ -14,5 +8,5 @@ ServerHost, ServerPort = "",50007
 while(1):
     conn, addr, ServerHost, ServerPort = TCP.EchoClient(ServerHost, ServerPort)
     date, date1, ClientIP, ClientPort, ServerHost, ServerPort, ReprData = TCP.EchoData(conn, addr, ServerHost, ServerPort)
-    Data = str(date) + " " + str(ClientIP) + ":" + str(ClientPort) + " " + str(ServerHost) + ":" + str(ServerPort) + "\n" + str(ReprData[1:-1]) + '\n'
+    Data = date + " " + str(ClientIP) + ":" + str(ClientPort) + " " + str(ServerHost) + ":" + str(ServerPort) + "\n" + ReprData[1:-1] + '\n'
     print Data
