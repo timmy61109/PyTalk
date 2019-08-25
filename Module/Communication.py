3998eded2d087e74096ed4902d1632d64e6d30d6
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-08-25 10:39:09 +0800

    如果採用，此提交將簡化通訊的套件
    修改項目:

--- Module/Communication.py
+++ Module/Communication.py
@@ -2,12 +2,6 @@
 # -*- coding: utf-8 -*-
 # coding = UTF-8
 import socket
-import demjson
-try:
-    from ElectricPowerBlockchainSystem.ElectricEnergyLoadModule.BlockFile import blockfile
-except:
-    from BlockFile import blockfile
-bf = blockfile()
 
 
 class tcp():
@@ -36,32 +30,3 @@ class tcp():
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             client.connect((Host, Port))
             client.sendall(transaction_pack.encode('utf8'))
-
-    def blockchain_client(
-            self, Host="127.0.0.1", Port=6000,
-            Path='../Data/objects/'):
-        blockchain = bf.read_blockchain(path=Path)
-        for block in blockchain:
-            self.client(Host=Host, Port=Port, transaction_pack=block)
-        self.client(Host=Host, Port=Port, transaction_pack="None")
-
-    def blockchain_server(self, Host="0.0.0.0", Port=6000):
-        """
-        接受檔案傳送過來
-        """
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-            server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-            server.bind((Host, Port))  # 開始監聽的IP與Port
-            server.listen(5)  # 設定連線上限5個
-            blockchain = list()
-            while True:
-                conn, addr = server.accept()
-                with conn:
-                    while True:
-                        data = conn.recv(4096)
-                        if not data:
-                            yield blockchain
-                            break
-                        else:
-                            block = data.decode('utf8')
-                            blockchain.append(block)
