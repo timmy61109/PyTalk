867d7b24cbec63c3aa694e3d9cd499ef6f705e82
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-08-25 10:57:44 +0800

    如果採用，此提交將更新接受方式
    修改項目:

--- talk_server.py
+++ talk_server.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # coding = UTF-8
-import TCP
-import FileAccess as FA
-#FileName = "./Data.csv"
-ServerHost, ServerPort = "",50007
-while(1):
-    conn, addr, ServerHost, ServerPort = TCP.EchoClient(ServerHost, ServerPort)
-    date, date1, ClientIP, ClientPort, ServerHost, ServerPort, ReprData = TCP.EchoData(conn, addr, ServerHost, ServerPort)
-    Data = date + " " + str(ClientIP) + ":" + str(ClientPort) + " " + str(ServerHost) + ":" + str(ServerPort) + "\n" + ReprData[1:-1] + '\n'
-    print Data
+from Module.Communication import tcp
+
+server_host, server_port = "", 50000
+data = tcp.server(Host=server_host, Port=server_port)
+while True:
+    message = next(data)
+    print(message)
