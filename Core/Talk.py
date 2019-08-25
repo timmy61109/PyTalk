12be9db7d326ee14f8f1f33e9c263e008e3edfbf
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-08-25 18:03:02 +0800

    如果採用，此提交將新增新的參數、修改回傳訊息
    修改項目:
    1. 使用新的參數讓客戶端使用結束訊息經過傳輸後可以結束伺服端的運作
    2. 將聊天核心使用的成功與否使用None和True來控制是否結束函式運作

--- Core/Talk.py
+++ Core/Talk.py
@@ -10,11 +10,14 @@ class talk():
     def __init__(self):
         self.version = "0.0.1"
 
-    def accept_message(self, host="0.0.0.0", port=5000, charset="utf8"):
-        data = tcp.server(Host=host, Port=port, Charset=charset)
+    def accept_message(self, host="0.0.0.0", port=5000, charset="utf8",
+                       end_of_message=True):
+        data = tcp.server(Host=host, Port=port,
+                          Charset=charset, end_of_message=end_of_message)
         while True:
             message = next(data)
             if message == "None":
+                yield None
                 break
             else:
                 yield message
@@ -22,9 +25,10 @@ class talk():
     def send_message(self, host="127.0.0.1", port=5000, charset="utf8"):
         while True:
             message = str(input("You:"))
+            tcp.client(Host=host, Port=port,
+                       message=message, Charset=charset)
             if message == "None":
-                yield "exit"
+                yield None
                 break
             else:
-                tcp.client(Host=host, Port=port,
-                           message=message, Charset=charset)
+                yield True
