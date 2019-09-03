1b7d9e014350b2154a480a68859d06e6d455ef1c
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-09-03 15:02:32 +0800

    如果採用，此提交將暫時修改內容
    修改項目:

--- ExpandApplication/PublicKeyCryptography.py
+++ ExpandApplication/PublicKeyCryptography.py
@@ -3,33 +3,25 @@ from fastecdsa.encoding.der import DEREncoder
 
 message = "a message to sign via ECDSA"  # 一些訊息
 
-"""
-使用預設曲線跟雜湊函數(P256 and SHA2)
-"""
+print("使用預設曲線P256跟雜湊函數SHA2")
 private_key = keys.gen_private_key(curve.P256)
 public_key = keys.get_public_key(private_key, curve.P256)
-print(private_key)
-print(public_key)
-"""
-產生數位簽章，為兩個整數r跟s。
-"""
+print("私密金鑰: " + str(private_key))
+print("公開金鑰: " + str(public_key))
+print(type(public_key))
+
+print("產生數位簽章，為兩個整數r跟s")
 r, s = ecdsa.sign(message, private_key)
 print(r)
 print(s)
-"""
-編碼數位簽章，使用DEREncoder為r跟s做編碼以利傳輸
-"""
+
+print("編碼數位簽章，使用DEREncoder為r跟s做編碼以利傳輸")
 encoded = DEREncoder.encode_signature(r, s)
-print(encoded)
+b = "數位簽章: "
+for i in encoded:
+    b += str(hex(i)) + " "
+print(b)
 decoded_r, decoded_s = DEREncoder.decode_signature(encoded)
-if r == decoded_r and s == decoded_s:
-    decoded_r, decoded_s
-    valid = ecdsa.verify((decoded_r, decoded_s), message, public_key)
-    print(valid)
-"""
-valid應該返回True，驗證前面產生的私密金鑰(private_key)、公開金鑰(public_key)、
-數位簽章(r, s)、加密訊息是有效的。
-"""
-public_key = b'X: 0x2b71ffb22f1e918e401f19c2ab4afa0fa9a4221c28c61cfde09dbbd28fb332b\nY: 0xdeb8a709e838a712156e3a5a8f189f4d1d1798de1f20417c5be65fc61606a384\n(On curve <P256>)'
 valid = ecdsa.verify((decoded_r, decoded_s), message, public_key)
-print(valid)
+if r == decoded_r and s == decoded_s and valid:
+    print("成功")
